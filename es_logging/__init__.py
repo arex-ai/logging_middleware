301dbe1e3f962158ae5743c451ec597c18913300
cristher <cristher@arex.ai>
2024-10-11 16:19:27 -0600

    Added a return response to log function

--- es_logging/__init__.py
+++ es_logging/__init__.py
@@ -1,1 +1,1 @@
-from es_logging.es_logging import configure_logging_api, log_context, logging_es, LogLevelEnum
+from es_logging.es_logging import configure_logging_api, log_context, logging_es, LogLevelEnum, LOGGING_API_URL
